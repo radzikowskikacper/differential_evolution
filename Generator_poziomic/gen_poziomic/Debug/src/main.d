a40dc957c0a4ad2851406414d27b45effb3d72ef
Yatamoto <yatamoto@gmail.com>
2014-05-05 16:37:19 +0200

    generowanie poziomic

---
+++ Generator_poziomic/gen_poziomic/Debug/src/main.d
@@ -0,0 +1,9 @@
+src/main.d: ../src/main.cpp ../src/../header/utils.hpp \
+ ../src/../header/../header/bitmap_image.hpp \
+ ../src/../header/bitmap_image.hpp
+
+../src/../header/utils.hpp:
+
+../src/../header/../header/bitmap_image.hpp:
+
+../src/../header/bitmap_image.hpp:
