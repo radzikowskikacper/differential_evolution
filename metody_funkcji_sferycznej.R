766ca3b174f90f0b5f963880000a89ec1b07310a
Yatamoto <yatamoto@gmail.com>
2014-06-07 17:42:07 +0200

    pare rzeczy

--- metody_funkcji_sferycznej.R
+++ metody_funkcji_sferycznej.R
@@ -9,9 +9,17 @@ evaluation<-function(coordinates)
   return(x1^2 + x2^2)
 }
 
+
+shift_p<-function(point)
+{
+  point[[1]][1] = point[[1]][1] + shift_v[1]
+  point[[1]][2] = point[[1]][2] + shift_v[2]
+  return (point)
+}
+
 genPointGP<-function()
 {
-  return (point(rnorm(2)))
+  return (shift_p(point(rnorm(2))))
 }
 
 
